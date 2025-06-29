39c90c1b47a8780759cebacef1999cb3460f332f
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2025-06-29 20:09:14 -0300

    [TASK-9] Fix linter settings and issues related

---
+++ eslint.config.mjs
@@ -0,0 +1,110 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+
+import typescriptParser from '@typescript-eslint/parser';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginTypescript from '@typescript-eslint/eslint-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        ignores: [
+            '.next/**',
+            '.cache/**',
+            'package-lock.json',
+            'public/**',
+            'node_modules/**',
+            'next-env.d.ts',
+            'next.config.ts',
+            'yarn.lock',
+            '**/*.d.ts',
+        ],
+    },
+
+    ...compat.extends(
+        'next/core-web-vitals',
+        'prettier',
+        'next',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ),
+
+    ...compat.env({
+        browser: true,
+        es2021: true,
+        node: true,
+    }),
+
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parser: typescriptParser,
+            ecmaVersion: 2021,
+            sourceType: 'module',
+        },
+        linterOptions: {
+            reportUnusedDisableDirectives: true,
+        },
+        plugins: {
+            prettier: pluginPrettier,
+            react: pluginReact,
+            'react-hooks': pluginReactHooks,
+            '@typescript-eslint': pluginTypescript,
+        },
+        rules: {
+            // JavaScript
+            'prefer-const': 'warn',
+            'no-var': 'warn',
+            'no-unused-vars': 'warn',
+            'object-shorthand': 'warn',
+            'quote-props': ['warn', 'as-needed'],
+
+            // TypeScript
+            '@typescript-eslint/array-type': ['warn', { default: 'array' }],
+            '@typescript-eslint/consistent-type-assertions': [
+                'warn',
+                {
+                    assertionStyle: 'as',
+                    objectLiteralTypeAssertions: 'never',
+                },
+            ],
+
+            // React
+            'react/jsx-fragments': ['warn', 'syntax'],
+            'react/jsx-filename-extension': [
+                'warn',
+                { extensions: ['ts', 'tsx'] },
+            ],
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+
+            // Prettier
+            'prettier/prettier': 'warn',
+
+            // Tailwind
+            'tailwindcss/classnames-order': 'off',
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+    },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,70 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-    <div className="w-screen min-h-screen px-10 py-8 background-design">
-        <header className="flex w-4/5 justify-between justify-self-center max-w-[1920px] items-center w-full z-10">
-            <Image className="cursor-pointer" src="/images/tissa.svg" alt="logo" width={75} height={32} />
-            <div className="flex items-center gap-8 color-neutral">
-                <Link aria-current="page" href="/" className="router-link-active router-link-exact-active cursor-pointer hover:text-violet-500 h-[44px] w-[100px] flex flex-col justify-center items-center active-nav-item">
-                    <span>Home</span>
-                    <div className="w-1/3 h-[3px] bg-violet-500 border-radius-full"></div>
-                </Link>
-                <a href="/portfolio" className="cursor-pointer hover:text-violet-500 h-[44px] w-[100px] flex flex-col justify-center items-center">
-                    <span>Portfolio</span>
-                </a>
-            </div>
-            <button type="button" className="text-white bg-gradient-to-r from-violet-500 via-violet-600 to-violet-700 hover:shadow-lg hover:shadow-violet-200 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-violet-300 dark:focus:ring-violet-800 font-medium rounded-full text-base px-5 py-2.5 text-center me-2 mb-2 ">
-                Contact me
-            </button>
-        </header>
-        <main className="flex flex-row gap-[32px] items-center sm:items-start">
-            <div className="flex flex-col gap-3 max-w-[500px] mt-5 ml-auto">
-                <div className="text-6xl font-black flex flex-col items-start relative">
-                    <div className="color-neutral z-10">Patricia</div>
-                    <div className="color-primary z-10">Coelho</div>
-                    <span className="color-neutral-light z-10 mt-2 text-xl font-light">Frontend Developer | FullStack Developer</span>
+        <div className='background-design min-h-screen w-screen px-10 py-8'>
+            <header className='z-10 flex w-4/5 w-full max-w-[1920px] items-center justify-between justify-self-center'>
+                <Image
+                    className='cursor-pointer'
+                    src='/images/tissa.svg'
+                    alt='logo'
+                    width={75}
+                    height={32}
+                />
+                <div className='color-neutral flex items-center gap-8'>
+                    <Link
+                        aria-current='page'
+                        href='/'
+                        className='router-link-active router-link-exact-active active-nav-item flex h-[44px] w-[100px] cursor-pointer flex-col items-center justify-center hover:text-violet-500'
+                    >
+                        <span>Home</span>
+                        <div className='border-radius-full h-[3px] w-1/3 bg-violet-500'></div>
+                    </Link>
+                    <a
+                        href='/portfolio'
+                        className='flex h-[44px] w-[100px] cursor-pointer flex-col items-center justify-center hover:text-violet-500'
+                    >
+                        <span>Portfolio</span>
+                    </a>
                 </div>
-                <div className="flex w-full gap-5 mt-10">
-                    <div className="bg-white hover:shadow-lg hover:shadow-blue-100 z-50 w-full cursor-pointer outline-0 hover:outline-1 hover:outline-blue-400 flex items-center justify-center gap-3 py-4 px-8 rounded-2xl">
-                        <Image src="/images/linkedin.svg" alt="LinkedIn" width={34} height={34} />
-                        <span className="color-neutral-dark">LinkedIn</span>
+                <button
+                    type='button'
+                    className='me-2 mb-2 rounded-full bg-gradient-to-r from-violet-500 via-violet-600 to-violet-700 px-5 py-2.5 text-center text-base font-medium text-white hover:bg-gradient-to-br hover:shadow-lg hover:shadow-violet-200 focus:ring-4 focus:ring-violet-300 focus:outline-none dark:focus:ring-violet-800'
+                >
+                    Contact me
+                </button>
+            </header>
+            <main className='flex flex-row items-center gap-[32px] sm:items-start'>
+                <div className='mt-5 ml-auto flex max-w-[500px] flex-col gap-3'>
+                    <div className='relative flex flex-col items-start text-6xl font-black'>
+                        <div className='color-neutral z-10'>Patricia</div>
+                        <div className='color-primary z-10'>Coelho</div>
+                        <span className='color-neutral-light z-10 mt-2 text-xl font-light'>
+                            Frontend Developer | FullStack Developer
+                        </span>
                     </div>
-                    <div className="bg-white hover:shadow-lg hover:shadow-blue-100 z-50 w-full cursor-pointer outline-0 hover:outline-1 hover:outline-blue-400 flex items-center justify-center gap-3 py-4 px-8 rounded-2xl">
-                        <Image src="/images/github.svg" alt="GitHub" width={34} height={34} />
-                        <span className="color-neutral-dark">GitHub</span>
+                    <div className='mt-10 flex w-full gap-5'>
+                        <div className='z-50 flex w-full cursor-pointer items-center justify-center gap-3 rounded-2xl bg-white px-8 py-4 outline-0 hover:shadow-lg hover:shadow-blue-100 hover:outline-1 hover:outline-blue-400'>
+                            <Image
+                                src='/images/linkedin.svg'
+                                alt='LinkedIn'
+                                width={34}
+                                height={34}
+                            />
+                            <span className='color-neutral-dark'>LinkedIn</span>
+                        </div>
+                        <div className='z-50 flex w-full cursor-pointer items-center justify-center gap-3 rounded-2xl bg-white px-8 py-4 outline-0 hover:shadow-lg hover:shadow-blue-100 hover:outline-1 hover:outline-blue-400'>
+                            <Image
+                                src='/images/github.svg'
+                                alt='GitHub'
+                                width={34}
+                                height={34}
+                            />
+                            <span className='color-neutral-dark'>GitHub</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex flex-col gap-3 min-w-[500px] max-w-[500px] mt-5 mr-auto">
-            </div>
-        </main>
-    </div>
+                <div className='mt-5 mr-auto flex max-w-[500px] min-w-[500px] flex-col gap-3'></div>
+            </main>
+        </div>
     );
 }
