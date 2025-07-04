3b86c94c0428cf03713227235cab0cb3c1ae866f
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2025-07-04 09:49:46 -0300

    [TASK-9] Refactor components and providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { ThemeProvider } from './ThemeProvider';
+import { ThemeProvider } from '../providers/ThemeProvider';
 
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
