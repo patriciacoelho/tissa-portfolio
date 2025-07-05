00e3a365b933b407eb78b32cce3bdc5088bcbde2
Patrícia Coelho <patriciacoelho.slv@gmail.com>
2025-07-05 02:05:28 -0300

    [TASK-9] Fix errors on build flow

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,3 +2,9 @@ export type Locale = (typeof locales)[number];
 
 export const locales = ['en', 'pt-br'] as const;
 export const defaultLocale: Locale = 'en';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    locales,
+    defaultLocale: 'en',
+};
